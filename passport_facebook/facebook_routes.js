aa0b278c0fc630b8032fb1a385fa4feb4ef02615
vaibhav bhardwaj <vaibhavbhardwaaj@gmail.com>
2019-05-05 18:00:12 +0530

    scope try

--- passport_facebook/facebook_routes.js
+++ passport_facebook/facebook_routes.js
@@ -3,7 +3,7 @@ const passport = require('passport');
 const route = express.Router();
 
 route.get('/facebook',
-  passport.authenticate('facebook'));
+  passport.authenticate('facebook',{scope:['user_location','user_photos']}));
 
 route.get('/facebook/redirect',passport.authenticate('facebook'),(req,res)=>{
   res.redirect('/dashbord');
