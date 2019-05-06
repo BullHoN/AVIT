66b29e8541a28dcaed9debbe540f464446b1abd0
vaibhav bhardwaj <vaibhavbhardwaaj@gmail.com>
2019-05-06 16:48:21 +0530

    facebook fix

--- passport_facebook/facebook_config.js
+++ passport_facebook/facebook_config.js
@@ -19,13 +19,13 @@ passport.use(new FacebookStrategy({
   clientSecret: 'b693b007e86a0d6ff96f24f90eaa3bda'
 },(accessToken,refreshToken,profile,done)=>{
   console.log(profile);
-  User.findOne({email:profile.id}).then((user)=>{
+  User.findOne({google_facebook_id:profile.id}).then((user)=>{
     if(user){
       console.log('user already existied');
       done(null,user);
     }else {
       const newuser = new User({
-        email:profile.id,
+        google_facebook_id:profile.id,
         isverified:true,
         username:'newuser_' + profile.id
       });
